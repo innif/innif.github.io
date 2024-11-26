a15d7018b64431e419a623b66e90b441fc0c2a12
Finn Harms <github@finn-harms.de>
2024-11-26 14:53:03 +0100

    added stuff

--- src/scheini-tools/src/router/index.js
+++ src/scheini-tools/src/router/index.js
@@ -7,7 +7,7 @@ import FullscreenClock from '../components/FullscreenClock.vue'
 import TimerView from '../components/Timer.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/scheini-tools/'),
   routes: [
     {
       path: '/',
